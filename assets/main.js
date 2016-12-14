f4c9bb940e968b54b5423da24f45f99ed122b980
aknj <aknjne@gmail.com>
2016-12-14 18:09:31 +0100

    build | step 5

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,23 @@
-$(function() {
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+  $.ajax({
+    url: 'https://www.codeschool.com/users/aknj.json',
+    dataType: 'jsonp',
+  })
+  .done(response => {
+    addCourses(response.courses.completed);
+  });
 
-  // your code will go here
+  function addCourses(courses) {
+    courses.forEach(course => {
+      console.log(course);
+      var courseDiv = document.createElement('div');
+      console.log(courseDiv);
+      courseDiv.setAttribute('class', 'course');
+      courseDiv.innerHTML = course.title;
+      document.getElementById('badges')
+              .appendChild(courseDiv);
+    });
+  }
 
 });
