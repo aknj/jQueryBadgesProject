8bca748b17a926574dc003112bbf9415859059bc
aknj <aknjne@gmail.com>
2016-12-14 18:21:07 +0100

    build | step 8&9 [complete]

--- assets/main.js
+++ assets/main.js
@@ -13,10 +13,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
     courses.forEach(course => {
       var courseDiv = document.createElement('div');
       courseDiv.setAttribute('class', 'course');
+      
       var h3 = document.createElement('h3');
       h3.innerText = course.title;
       courseDiv.appendChild(h3);
+      
+      var img = document.createElement('img');
+      img.setAttribute('src', course.badge);
+      courseDiv.appendChild(img);
 
+      var a = document.createElement('a');
+      a.setAttribute('href', course.url);
+      a.setAttribute('target', '_blank');
+      a.setAttribute('class', 'btn btn-primary');
+      a.innerText = 'See Course';
+      courseDiv.appendChild(a);
 
       // finally add the element
       badgesDiv.appendChild(courseDiv);
